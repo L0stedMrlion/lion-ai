5a6fb5c87e352e75767f700ff69a601b4879524f
Mrlion <mrlion4202@gmail.com>
2023-12-01 20:35:33 +0000

    some changes

--- src/AI.py
+++ src/AI.py
@@ -1,2 +1,17 @@
-print("Lion AI - Starting up")
-print("Lion AI - Succesfully started!")
+import os
+import asyncio
+from OpenAI import AsyncOpenAI
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+response = client.completions.create(
+  model="gpt-3.5-turbo-instruct",
+  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
+  temperature=0.9,
+  max_tokens=64,
+  top_p=1,
+  presence_penalty=0.6,
+  stop=[" Human:", " AI:"]
+)
