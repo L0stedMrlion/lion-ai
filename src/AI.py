45b64b46fb982e5aea93ccf0301bc45e7550e4e4
Mrlion <mrlion4202@gmail.com>
2023-12-01 20:39:15 +0000

    some changes

--- src/AI.py
+++ src/AI.py
@@ -1,17 +1,14 @@
-import os
-import asyncio
-from OpenAI import AsyncOpenAI
+import openai
 
-client = OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
+openai.api_key = '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'
+
+prompt = "Translate the following English text to French:"
+
+response = openai.Completion.create(
+    engine="text-davinci-002",
+    prompt=prompt,
+    max_tokens=100
 )
 
-response = client.completions.create(
-  model="gpt-3.5-turbo-instruct",
-  prompt="The following is a conversation with an AI assistant. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: Hello, who are you?\nAI: I am an AI created by OpenAI. How can I help you today?\nHuman: I'd like to cancel my subscription.\nAI:",
-  temperature=0.9,
-  max_tokens=64,
-  top_p=1,
-  presence_penalty=0.6,
-  stop=[" Human:", " AI:"]
-)
+generated_text = response['choices'][0]['text']
+print(generated_text)
